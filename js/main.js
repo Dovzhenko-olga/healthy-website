cbc45e4aa9d5fa6453bc463f0155d66db4a330ee
Dovzhenko-olga <73553889+Dovzhenko-olga@users.noreply.github.com>
2021-10-20 21:28:54 +0300

    finish work

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('.hero__movie').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+
+  $('.experts__movie-link').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+
+  $('.menu__burger-btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+});
